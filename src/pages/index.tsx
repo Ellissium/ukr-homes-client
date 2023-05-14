ae962ed09bda8af78864d2dfa50ae28f8d0017f0
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-14 19:11:55 +0200

    Create basic routing

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { setCategory, setPage } from '@/store/page/pageFilters.slice'
+
+import { TypePaginationPosts } from '@/types/post.interface'
+
+import Home from '@/screens/home/Home'
+
+const HomePage: NextPage<TypePaginationPosts> = () => {
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(setPage(1))
+		dispatch(setCategory(undefined))
+	}, [])
+	return <Home />
+}
+
+export default HomePage
