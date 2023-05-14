994f824a6b4a3e18cf6d56fbe4e968e2f56fb308
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-14 20:35:50 +0200

    Create custom heading.

---
+++ src/components/ui/heading/Heading.tsx
@@ -0,0 +1,9 @@
+import { FC, PropsWithChildren } from 'react'
+
+import styles from './Heading.module.scss'
+
+const Heading: FC<PropsWithChildren> = ({ children }) => {
+	return <h1 className={styles.heading}>{children}</h1>
+}
+
+export default Heading
