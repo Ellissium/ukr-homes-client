2212c99d0a46ddef0d3c1ad08edd526ce11aa823
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-14 20:34:18 +0200

    Create custom button.

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,23 @@
+import cn from 'clsx'
+import { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react'
+
+import styles from './Button.module.scss'
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+	variant?: '' | 'ghost' | 'search'
+
+	active?: boolean
+}
+const Button: FC<PropsWithChildren<IButton>> = ({
+	children,
+	variant = '',
+	...rest
+}) => {
+	return (
+		<button {...rest} className={cn(styles.button, styles[variant])}>
+			{children}
+		</button>
+	)
+}
+
+export default Button
