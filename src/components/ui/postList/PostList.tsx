802fd6786f8f7c5a5b3ffcad96ead0f262b5fc54
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-16 01:04:18 +0200

    Create post list component.

--- src/components/ui/postList/PostList.tsx
+++ src/components/ui/postList/PostList.tsx
@@ -1,7 +1,21 @@
 import { FC } from 'react'
 
-const PostList: FC = () => {
-	return <div>PostList</div>
+import { TypePaginationPosts } from '@/types/post.interface'
+
+import PostCard from '../postCard/PostCard'
+
+import styles from './PostList.module.scss'
+
+const PostList: FC<TypePaginationPosts> = data => {
+	return (
+		<div>
+			<div className={styles.home__postList}>
+				{data?.posts?.map(post => (
+					<PostCard key={post.id} post={post}></PostCard>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default PostList
