9d38110fcaf4374e2e7accdb5393a569bea8b117
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 21:16:16 +0200

    Modify auth component

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from 'next'
 
+import Meta from '@/ui/meta/Meta'
+
 import Auth from '@/screens/auth/Auth'
 
 const AuthPage: NextPage = () => {
-	return <Auth />
+	return (
+		<Meta title='Auth'>
+			<Auth />
+		</Meta>
+	)
 }
 
 export default AuthPage
