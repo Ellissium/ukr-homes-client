6ea7e39c92d7049cc3f3e955fbd029f4b8054ff0
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 21:21:40 +0200

    Modify refugee page

--- src/pages/posts/refugee/index.tsx
+++ src/pages/posts/refugee/index.tsx
@@ -2,19 +2,62 @@ import { NextPage } from 'next'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { setCategory, setPage } from '@/store/page/pageFilters.slice'
+import Layout from '@/ui/layout/Layout'
+import Meta from '@/ui/meta/Meta'
+
+import {
+	setCategory,
+	setMaxArea,
+	setMaxBathRooms,
+	setMaxBeds,
+	setMaxFloor,
+	setMaxPrice,
+	setMaxRent,
+	setMaxRooms,
+	setMinArea,
+	setMinBathRooms,
+	setMinBeds,
+	setMinFloor,
+	setMinPrice,
+	setMinRent,
+	setMinRooms,
+	setPage,
+	setRegion
+} from '@/store/page/pageFilters.slice'
 
 import Home from '@/screens/home/Home'
+import { SearchRegionEnum } from '@/services/category/dropdownEnum.types'
 import { EnumPostCategory } from '@/services/post/post.types'
 
-const PostsRentPage: NextPage = () => {
+const PostsRefugeePage: NextPage = () => {
 	const dispatch = useDispatch()
 	useEffect(() => {
 		dispatch(setPage(1))
 		dispatch(setCategory(EnumPostCategory.REFUGEE))
+		dispatch(setMinPrice(undefined))
+		dispatch(setMaxPrice(undefined))
+		dispatch(setMinArea(undefined))
+		dispatch(setMaxArea(undefined))
+		dispatch(setMinFloor(undefined))
+		dispatch(setMaxFloor(undefined))
+		dispatch(setMinRooms(undefined))
+		dispatch(setMaxRooms(undefined))
+		dispatch(setMinRent(undefined))
+		dispatch(setMaxRent(undefined))
+		dispatch(setMinBeds(undefined))
+		dispatch(setMaxBeds(undefined))
+		dispatch(setMinBathRooms(undefined))
+		dispatch(setMaxBathRooms(undefined))
+		dispatch(setRegion(SearchRegionEnum[0].key))
 	}, [])
 
-	return <Home />
+	return (
+		<Meta title='Refugee'>
+			<Layout hideDistance={600}>
+				<Home />
+			</Layout>
+		</Meta>
+	)
 }
 
-export default PostsRentPage
+export default PostsRefugeePage
