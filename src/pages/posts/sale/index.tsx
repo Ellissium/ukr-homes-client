004452712108e194fb7bfaaf190acc1f0c15221b
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 21:20:30 +0200

    Modify sale page

--- src/pages/posts/sale/index.tsx
+++ src/pages/posts/sale/index.tsx
@@ -2,11 +2,33 @@ import { NextPage } from 'next'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { setCategory, setPage } from '@/store/page/pageFilters.slice'
+import Layout from '@/ui/layout/Layout'
+import Meta from '@/ui/meta/Meta'
+
+import {
+	setCategory,
+	setMaxArea,
+	setMaxBathRooms,
+	setMaxBeds,
+	setMaxFloor,
+	setMaxPrice,
+	setMaxRent,
+	setMaxRooms,
+	setMinArea,
+	setMinBathRooms,
+	setMinBeds,
+	setMinFloor,
+	setMinPrice,
+	setMinRent,
+	setMinRooms,
+	setPage,
+	setRegion
+} from '@/store/page/pageFilters.slice'
 
 import { TypePaginationPosts } from '@/types/post.interface'
 
 import Home from '@/screens/home/Home'
+import { SearchRegionEnum } from '@/services/category/dropdownEnum.types'
 import { EnumPostCategory } from '@/services/post/post.types'
 
 const PostsSalePage: NextPage<TypePaginationPosts> = () => {
@@ -14,9 +36,30 @@ const PostsSalePage: NextPage<TypePaginationPosts> = () => {
 	useEffect(() => {
 		dispatch(setPage(1))
 		dispatch(setCategory(EnumPostCategory.SALE))
+		dispatch(setMinPrice(undefined))
+		dispatch(setMaxPrice(undefined))
+		dispatch(setMinArea(undefined))
+		dispatch(setMaxArea(undefined))
+		dispatch(setMinFloor(undefined))
+		dispatch(setMaxFloor(undefined))
+		dispatch(setMinRooms(undefined))
+		dispatch(setMaxRooms(undefined))
+		dispatch(setMinRent(undefined))
+		dispatch(setMaxRent(undefined))
+		dispatch(setMinBeds(undefined))
+		dispatch(setMaxBeds(undefined))
+		dispatch(setMinBathRooms(undefined))
+		dispatch(setMaxBathRooms(undefined))
+		dispatch(setRegion(SearchRegionEnum[0].key))
 	}, [])
 
-	return <Home />
+	return (
+		<Meta title='Sale'>
+			<Layout hideDistance={600}>
+				<Home />
+			</Layout>
+		</Meta>
+	)
 }
 
 export default PostsSalePage
