604a5d410307bf3e6a7b4ea758299037c4d924f2
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 20:59:27 +0200

    Modify postCard component

--- src/components/ui/postCard/PostCard.tsx
+++ src/components/ui/postCard/PostCard.tsx
@@ -3,26 +3,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC, useRef, useState } from 'react'
-import { FiHeart } from 'react-icons/fi'
+import { FiEdit, FiTrash2 } from 'react-icons/fi'
 
 import { IPost } from '@/types/post.interface'
 
+import FavoriteButton from '../favoriteButton/FavoriteButton'
+
 import styles from './PostCard.module.scss'
-import postImage from '@/../public/images/home_interior.jpg'
+import { RegionEnum } from '@/services/category/dropdownEnum.types'
 
 interface PostCardProps {
+	handleDeletePost?: (postId: number | string) => void
+	isProfilePost: boolean
 	post: IPost
 }
-const PostCard: FC<PostCardProps> = ({ post }) => {
-	const utilitiesPrice = 1000
-	const depositPrice = 1000
-	const totalPrice = 3000
+
+const PostCard: FC<PostCardProps> = ({
+	handleDeletePost,
+	isProfilePost,
+	post
+}) => {
 	const router = useRouter()
 	const overlayRef = useRef<HTMLDivElement>(null)
-
 	const [overlayPosition, setOverlayPosition] = useState({ x: 0, y: 0 })
 	const [overlayVisible, setOverlayVisible] = useState(false)
-
 	const handleMouseMove = (e: any) => {
 		e.preventDefault()
 		setTimeout(() => {
@@ -44,35 +48,43 @@ const PostCard: FC<PostCardProps> = ({ post }) => {
 			}, 300)
 		}
 	}
-
+	const handleDelete = (e: any) => {
+		e.stopPropagation()
+		console.log('delete')
+		if (handleDeletePost) {
+			handleDeletePost(post.id)
+		}
+	}
 	return (
-		<Link
-			href={`/posts/${post.category.slug}/${post.slug}`}
-			passHref
-			className={styles.postCard}
-			onClick={handleMouseMove}
-		>
+		<div className={styles.postCard} onClick={handleMouseMove}>
 			<div className={styles.postCard__container}>
 				<div className={styles.postCard__gallery}>
 					<Image
-						src={postImage}
+						src={post.images[0]}
+						width={1000}
+						height={1000}
 						alt='My Image'
 						className={styles.postCard__galleryImage}
 					></Image>
 				</div>
 				<div className={styles.postCard__information}>
 					<div className={styles.postCard__description}>
-						<div className={styles.postCard__textHeaders}>
-							{post.id + ' ' + post.name}
+						<div>
+							<div className={styles.postCard__textMainHeaders}>
+								{'№' + post.id + ' ' + post.name}
+							</div>
+							<div className={styles.postCard__text}>
+								{post.city +
+									', ' +
+									post.street +
+									' ' +
+									post.houseNumber +
+									', ' +
+									RegionEnum.find(item => item.key === post.region)?.value}
+							</div>
 						</div>
-						<div className={styles.postCard__text}>
-							{post.region +
-								' ' +
-								post.city +
-								' ' +
-								post.street +
-								' ' +
-								post.houseNumber}
+						<div className={styles.postCard__textDescription}>
+							{post.description}
 						</div>
 						<div className={styles.postCard__text}>
 							{post.area +
@@ -84,25 +96,72 @@ const PostCard: FC<PostCardProps> = ({ post }) => {
 						</div>
 					</div>
 					<div className={styles.postCard__priceList}>
-						<span className={styles.postCard__textHeaders}>rent:</span>
-						<span className={styles.postCard__text}>{post.price + ' $'}</span>
-						<span className={styles.postCard__textHeaders}>utilities:</span>
-						<span className={styles.postCard__text}>
-							{utilitiesPrice + ' $'}
+						<span className={styles.postCard__textHeaders}>
+							{post.category.id === 2 ? 'Ціна за м²' : 'Комунальні послуги:'}:
 						</span>
-						<span className={styles.postCard__textHeaders}>deposit:</span>
-						<span className={styles.postCard__text}>{depositPrice + ' $'}</span>
+						<span className={styles.postCard__text}>
+							{post.category.id === 2
+								? (post.price / post.area).toFixed(2) + ' ₴'
+								: post.utilitiesPrice + ' ₴'}
+						</span>
+						{post.category.id === 2 && (
+							<div className={styles.postCard__utilitiesContainer}>
+								<span className={styles.postCard__textHeaders}>
+									Додаткові витрати:
+								</span>
+								<span className={styles.postCard__text}>
+									{post.utilitiesPrice + ' ₴'}
+								</span>{' '}
+							</div>
+						)}
+						{post.category.id !== 2 && (
+							<div className={styles.postCard__utilitiesContainer}>
+								<span className={styles.postCard__textHeaders}>Ціна:</span>
+								<span className={styles.postCard__text}>
+									{post.price + ' ₴'}
+								</span>{' '}
+							</div>
+						)}
 					</div>
 					<div className={styles.postCard__total}>
-						<div className={styles.postCard__favorite}>
-							<FiHeart
-								className={styles.postCard__favoriteIcon}
-								onClick={e => e.stopPropagation()}
-							/>
+						<div className={styles.postCard__totalButtons}>
+							<FavoriteButton productId={post.id}></FavoriteButton>
+
+							{isProfilePost && (
+								<Link
+									className={styles.postCard__edit}
+									href={`/posts/newPost?id=${post.id}`}
+								>
+									<FiEdit
+										className={styles.postCard__editIcon}
+										onClick={e => e.stopPropagation()}
+									/>
+								</Link>
+							)}
+							{isProfilePost && (
+								<div className={styles.postCard__bin} onClick={handleDelete}>
+									<FiTrash2 className={styles.postCard__binIcon} />
+								</div>
+							)}
 						</div>
+
 						<div className={styles.postCard__totalPrice}>
-							<span className={styles.postCard__textHeaders}>total:</span>
-							<span className={styles.postCard__text}>{totalPrice}</span>
+							{post.category.id !== 2 ? (
+								<>
+									<span className={styles.postCard__textHeaders}>Разом:</span>
+									<span className={styles.postCard__text}>
+										{post.price + post.utilitiesPrice + ' ₴'}
+									</span>
+								</>
+							) : (
+								<>
+									<span className={styles.postCard__textHeaders}>Ціна:</span>
+									<span className={styles.postCard__text}>
+										{(post.price / post.area + post.utilitiesPrice).toFixed(2) +
+											' ₴'}
+									</span>
+								</>
+							)}
 						</div>
 					</div>
 				</div>
@@ -117,7 +176,7 @@ const PostCard: FC<PostCardProps> = ({ post }) => {
 					transform: `translate(${overlayPosition.x}px, ${overlayPosition.y}px)`
 				}}
 			/>
-		</Link>
+		</div>
 	)
 }
 
