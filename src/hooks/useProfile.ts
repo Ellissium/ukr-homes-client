a7fe7737928b770f6f0a9028eedd7aeb1246832e
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 20:43:12 +0200

    Modify UseProfile hook

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,10 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 
+import { errorCatch } from '@/api/api_helper'
+
 import { UserService } from '@/services/user/user.service'
 
 export const useProfile = () => {
-	const { data } = useQuery(['get profile'], () => UserService.getProfile(), {
-		select: data => data
-	})
+	console.log('invalid')
+	const { data } = useQuery(
+		['get profile'],
+		async () => await UserService.getProfile(),
+		{
+			select: data => data,
+			onError: error => {
+				console.log(errorCatch(error))
+			}
+		}
+	)
 	return data
 }
