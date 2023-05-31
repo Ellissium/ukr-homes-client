5c5a6b2156d4da4fb6b8ba0fcfbe038273269dfa
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 21:19:45 +0200

    Modify rent page

--- src/pages/posts/rent/index.tsx
+++ src/pages/posts/rent/index.tsx
@@ -2,9 +2,31 @@ import { NextPage } from 'next'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { setCategory, setPage } from '@/store/page/pageFilters.slice'
+import Layout from '@/ui/layout/Layout'
+import Meta from '@/ui/meta/Meta'
+
+import {
+	setCategory,
+	setMaxArea,
+	setMaxBathRooms,
+	setMaxBeds,
+	setMaxFloor,
+	setMaxPrice,
+	setMaxRent,
+	setMaxRooms,
+	setMinArea,
+	setMinBathRooms,
+	setMinBeds,
+	setMinFloor,
+	setMinPrice,
+	setMinRent,
+	setMinRooms,
+	setPage,
+	setRegion
+} from '@/store/page/pageFilters.slice'
 
 import Home from '@/screens/home/Home'
+import { SearchRegionEnum } from '@/services/category/dropdownEnum.types'
 import { EnumPostCategory } from '@/services/post/post.types'
 
 const PostsRentPage: NextPage = () => {
@@ -12,9 +34,30 @@ const PostsRentPage: NextPage = () => {
 	useEffect(() => {
 		dispatch(setPage(1))
 		dispatch(setCategory(EnumPostCategory.RENT))
+		dispatch(setMinPrice(undefined))
+		dispatch(setMaxPrice(undefined))
+		dispatch(setMinArea(undefined))
+		dispatch(setMaxArea(undefined))
+		dispatch(setMinFloor(undefined))
+		dispatch(setMaxFloor(undefined))
+		dispatch(setMinRooms(undefined))
+		dispatch(setMaxRooms(undefined))
+		dispatch(setMinRent(undefined))
+		dispatch(setMaxRent(undefined))
+		dispatch(setMinBeds(undefined))
+		dispatch(setMaxBeds(undefined))
+		dispatch(setMinBathRooms(undefined))
+		dispatch(setMaxBathRooms(undefined))
+		dispatch(setRegion(SearchRegionEnum[0].key))
 	}, [])
 
-	return <Home />
+	return (
+		<Meta title='Rent'>
+			<Layout hideDistance={600}>
+				<Home />
+			</Layout>
+		</Meta>
+	)
 }
 
 export default PostsRentPage
