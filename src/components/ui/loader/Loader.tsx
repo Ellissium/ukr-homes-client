01a0785cc252da795d9a9a0800151d0b35301ab0
Elisey Cherkov <warriorofsparta252@gmail.com>
2023-05-31 20:57:45 +0200

    Modify loader component

--- src/components/ui/loader/Loader.tsx
+++ src/components/ui/loader/Loader.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 import styles from './Loader.module.scss'
+import walk from '@/../public/images/walk.gif'
 
 const Loader: FC = () => {
-	return <div className={styles.loader}></div>
+	return (
+		<div className={styles.loader}>
+			<div className={styles.loader__item}>
+				<Image
+					priority={true}
+					src={walk}
+					alt='My Image'
+					width={40}
+					height={40}
+					className={styles.loader__image}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Loader
